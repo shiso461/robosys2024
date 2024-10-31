47a9bf28122a5bf7cf0c508814f6aeb3e476ea3e
Soma Shirai <shiso461@gmail.com>
2024-10-31 13:11:20 +0900

    change test

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Soma Shirai <shiso461@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -10,7 +10,15 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO"
+[ "${out}" = 15 ] || ng "$LINENO" 
 
-[ "${res}" = 0 ] && echo OK
+out=$(seq あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo ok
 exit "$res"
