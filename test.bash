760a51bc9c655db1d0ac267fb658438752b280d8
Soma Shirai <shiso461@gmail.com>
2024-11-07 11:56:47 +0900

    change

--- test.bash
+++ test.bash
@@ -3,14 +3,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよ
-	res=1
+        echo ${1}行目が違うよ
+        res=1
 }
 
 res=0
 
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "$LINENO" 
+[ "${out}" = 15 ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo ok
 exit "$res"
