f80cb07bb5564499e3cd16840c74324e1762a516
Soma Shirai <shiso461@gmail.com>
2024-11-26 22:42:06 +0900

    追加したコマンドのテストを追加

--- test.bash
+++ test.bash
@@ -9,6 +9,8 @@ ng () {
 
 res=0
 
+
+### plusコマンドへのテスト###
 ### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
@@ -21,6 +23,39 @@ out=$(seq あ | ./plus)
 out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
+
+
+### prime_fコマンドへのテスト###
+# 10の素因数分解
+out=$(echo "10" | ./prime_f)
+expected="10: 2 * 5"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+# 100の素因数分解
+out=$(echo "100" | ./prime_f)
+expected="100: 2 * 2 * 5 * 5"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+# 1が入力された場合
+out=$(echo "1" | ./test)
+expected="1 は素因数分解できません。"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+# マイナスの数値が入力された場合
+out=$(echo "-10" | ./test)
+expected="-10は無効な入力です。"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+# 少数が入力された場合
+out=$(echo "1.5" | ./test)
+expected="1.5は無効な入力です。"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
+# 数値以外が入力された場合
+out=$(echo "abc" | ./test)
+expected="abc は無効な入力です。"
+[ "${out}" = "${expected}" ] || ng "$LINENO"
+
 
 [ "$res" = 0 ] && echo ok
 exit "$res"
